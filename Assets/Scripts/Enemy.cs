25501abf5c26c0797ad4584679d4696cc4b020c1
Joe Glennon <joseph.glennon1@gmail.com>
2021-01-09 16:02:35 -0500

    Enemies destroy on collision

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Tooltip("Particle prefab on Enenmy Ship")]
+    [SerializeField] GameObject m_DeathFX;
+
+    float m_DestroyDelay = .5f;
+
+
+    private void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+       Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        m_DeathFX.SetActive(true);
+        Invoke("DestroyShip", m_DestroyDelay);
+    }
+
+    void DestroyShip()
+    {
+        Destroy(gameObject);
+    }
+
+}
